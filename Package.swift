40b6831ce981dc234dd4b0c028d37f75398a1e7c
kimjaehyeon-nbt <kim.jaehyeon@nbt.com>
2023-10-03 11:39:01 +0900

    1.1.8 릴리즈

--- Package.swift
+++ Package.swift
@@ -4,20 +4,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "adison-ofw-module-paybooc-ios",
+    name: "PayboocOfwModule",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "adison-ofw-module-paybooc-ios",
-            targets: ["adison-ofw-module-paybooc-ios"]),
+            name: "PayboocOfwModule",
+            targets: ["PayboocOfwModule"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/adison-ads/adison-offerwall-ios-sdk", exact: Version("3.1.48"))
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "adison-ofw-module-paybooc-ios"),
-        .testTarget(
-            name: "adison-ofw-module-paybooc-iosTests",
-            dependencies: ["adison-ofw-module-paybooc-ios"]),
+        .binaryTarget(
+            name: "PayboocOfwModule",
+            url: "https://github.com/adison-ads/adison-ofw-module-paybooc-ios/releases/download/v1.1.8/PayboocOfwModule.zip",
+            checksum: "5443ee0ea11f67e0fab4dfa95669f0817d82029c5f39c90d5708c65818809ec6"
+        )
     ]
 )
