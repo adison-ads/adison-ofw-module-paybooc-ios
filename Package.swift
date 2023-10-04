e691d99af86ae78650111c017b178f231dd18f14
kimjaehyeon-nbt <kim.jaehyeon@nbt.com>
2023-10-04 14:32:37 +0900

    Package 파일 수정

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,19 +9,26 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PayboocOfwModule",
-            targets: ["PayboocOfwModule"]
+            targets: ["PayboocOfwModule", "PayboocOfwModuleTarget"]
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/adison-ads/adison-offerwall-ios-sdk", exact: Version("3.1.48"))
+        .package(
+            name: "AdisonOfferwallSDK",
+            url: "https://github.com/adison-ads/adison-offerwall-ios-sdk",
+            .upToNextMajor(from: Version("3.1.48"))
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "PayboocOfwModule",
             url: "https://github.com/adison-ads/adison-ofw-module-paybooc-ios/releases/download/v1.1.8/PayboocOfwModule.zip",
             checksum: "5443ee0ea11f67e0fab4dfa95669f0817d82029c5f39c90d5708c65818809ec6"
+        ),
+        .target(
+            name: "PayboocOfwModuleTarget",
+            dependencies: ["AdisonOfferwallSDK"],
+            path: "Sources"
         )
     ]
 )
---
+++ Tests/PayboocOfwModule-iosTests/PayboocOfwModuleTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import PayboocOfwModule
+
+final class PayboocOfwModuleTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(PayboocOfwModule().text, "Hello, World!")
+    }
+}
